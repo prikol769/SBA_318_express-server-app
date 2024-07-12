befa65fab2fe4e4686bc4d640a9b8b898aef0e0e
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-12 12:04:12 -0500

    added data

---
+++ data/comments.js
@@ -0,0 +1,58 @@
+const comments = [
+  {
+    id: 1,
+    userId: 1,
+    postId: 3,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit eum accusamus sed, eius cumque nobis qui porro sint ratione doloremque? Eos, dolorem voluptatem. Eligendi animi ad eos magnam veritatis",
+  },
+  {
+    id: 2,
+    userId: 1,
+    postId: 4,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde dicta ex similique.",
+  },
+  {
+    id: 3,
+    userId: 2,
+    postId: 3,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis accusamus suscipit ea, veniam, officia nisi voluptate modi labore eum ab quia dolorem? Perferendis ipsa sit consectetur veritatis. Pariatur, aliquam corrupti!",
+  },
+  {
+    id: 4,
+    userId: 2,
+    postId: 5,
+    text: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 5,
+    userId: 2,
+    postId: 2,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione officiis tenetur reprehenderit eos. Dolor, vitae libero! Atque consequuntur nisi dolor vitae laboriosam corporis nobis assumenda!",
+  },
+  {
+    id: 6,
+    userId: 2,
+    postId: 7,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, sapiente blanditiis.",
+  },
+  {
+    id: 7,
+    userId: 3,
+    postId: 1,
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem libero itaque odit suscipit, illum hic earum inventore! Aut, quas.",
+  },
+  {
+    id: 8,
+    userId: 3,
+    postId: 3,
+    text: "Lorem ipsum dolor sit amet consectetur.",
+  },
+  {
+    id: 9,
+    userId: 3,
+    postId: 5,
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo culpa, quod iste commodi, debitis enim nisi delectus obcaecati itaque quaerat quibusdam repellat minima corrupti, quam explicabo. Repellendus dolor numquam obcaecati, facere, reprehenderit corporis tempora quis sed commodi blanditiis ipsam nostrum laborum repudiandae porro accusamus officia. Nobis enim numquam excepturi minima quos, cum veritatis vero vel.",
+  },
+];
+
+module.exports = comments;
